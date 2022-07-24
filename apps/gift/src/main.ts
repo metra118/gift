092b050bb0a21b664d4f73940bb2da28dfd62d78
metra <miraclemarvel118@gmail.com>
2022-07-24 10:08:50 +0300

    1. user in microservices nest createApplicationContext because they don't use http 2. update nest packages

--- apps/gift/src/main.ts
+++ apps/gift/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app/app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.createApplicationContext(AppModule)
   await app.init()
   console.log('gift was started')
 }
